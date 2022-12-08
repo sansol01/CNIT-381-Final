ea9f9038a236d5d355c12e0eaffefb5cf81612df
berendsr <118387714+berendsr@users.noreply.github.com>
2022-12-08 11:14:10 -0600

    Update monitor_skill.py

--- monitor_skill.py
+++ monitor_skill.py
@@ -1,14 +1,22 @@
-
 import os
 import sys
 import subprocess
 
-with open("CSR1.txt","r") as temp:
-    test=temp.readlines()
-pinged=test[3]
 
-ping_result=pinged[29:32]
+#Updates the interface with new ip and sends a ping.
+def ping():
+    x=1
+    if x == 1:
+        os.system("sh ping_run.sh")
 
-required_ping = "5/5"
-if ping_result != required_ping:
-   os.system("sh monitor_skill_run.sh")
+#Monitors ping results and updates vpn configuration
+def monitor():
+    with open("CSR1.txt","r") as temp:
+        test=temp.readlines()
+    pinged=test[3]
+    ping_result=pinged[29:32]
+    required_ping = "5/5"
+    if ping_result != required_ping:
+        os.system("sh monitor_skill_run.sh")
+
+
