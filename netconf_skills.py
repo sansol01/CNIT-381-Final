3930d9261c48452ce15487a43a38f7e22c75b373
JacobSletten <118213062+JacobSletten@users.noreply.github.com>
2022-12-08 18:23:46 -0600

    Update netconf_skills.py

--- netconf_skills.py
+++ netconf_skills.py
@@ -2,41 +2,24 @@ from ncclient import manager
 import xml.dom.minidom
 import routers
 
-#r1 = manager.connect(**routers.router1)
-#r2 = manager.connect(**routers.router2)
-
-
-r1 = manager.connect(
-    host="192.168.56.101",
-    port=830,
-    username="cisco",
-    password="cisco123!",
-    hostkey_verify=False
-)
-
-r2 = manager.connect(
-    host="192.168.56.103",
-    port=830,
-    username="cisco",
-    password="cisco123!",
-    hostkey_verify=False
-)
+HQ = manager.connect(**routers.ncHQ)
+Branch = manager.connect(**routers.ncBranch)
 
 # import templates
-int_off_template = open("interfaces/int_switch_off.j2").read()
-int_on_template = open("interfaces/int_switch_on.j2").read()
-int_filter = open("interfaces/int_filter.j2").read()
-new_loop = open("interfaces/new_loop.j2").read()
+int_off_template = open("templates/int_switch_off.j2").read()
+int_on_template = open("templates/int_switch_on.j2").read()
+int_filter = open("templates/int_filter.j2").read()
+new_loop = open("templates/new_loop.j2").read()
 
 #Configures a loopback interface on router one for demo purposes
 def conf_loopback(number): 
     loop_config = new_loop.format(num=number[9])
     print(loop_config)
     #Create the interface using the configuration template
-    netconf_reply = r1.edit_config(target="running", config=loop_config)
+    netconf_reply = HQ.edit_config(target="running", config=loop_config)
     print(netconf_reply)
     #Use a filter to only show interface name and description
-    netconf_reply = r1.get_config(source="running", filter = int_filter.format(full_int_name = ("Loopback" + number[9])))
+    netconf_reply = HQ.get_config(source="running", filter = int_filter.format(full_int_name = ("Loopback" + number[9])))
     return xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml() #Return the reply
 
 
@@ -49,7 +32,7 @@ Checks weather an interface is up or down.
 Returns: 1 if enabled, 0 if disabled
 '''
 def check_interface(interface,rTarget):
-    #get the target interface's configuration 
+    #get the target interface's configuration
     netconf_reply = str(rTarget.get_config(source="running", filter = interface))
     #check if the interface is enabled
     if "<enabled>true</enabled>" in netconf_reply:
@@ -57,36 +40,38 @@ def check_interface(interface,rTarget):
     else: 
         return 0
 
-
-
 # Determines what router and interface should be toggled based on the arguments.
-def toggle_handler(message):
+def toggle_handler(message): 
     #cut toggle out of the string
-    args = message[6:]
+    args = message.split(" ")
     
     #check for a target interface
-    if "lo2" in args:
+    if "lo1" in args[2].lower():
+        intName = "Loopback"
+        intNum = "1"
+    elif "lo2" in args[2].lower():
         intName = "Loopback"
         intNum = "2"
-    elif "lo3" in args:
+    elif "lo3" in args[2].lower():
         intName = "Loopback"
         intNum = "3"
-    elif "gb1" in args:
+    elif "gb1" in args[2].lower():
         intName = "GigabitEthernet"
         intNum = "1"
-    elif "gb2" in args:
+    elif "gb2" in args[2].lower():
         intName = "GigabitEthernet"
         intNum = "2"
-    else: #default to lo1
-        intName = "Loopback"
-        intNum = "1"
+    else: 
+        return "Invalid Interface"
 
     #Check for a target router
-    if "r2" in args:
-        rTarget = r2
-    else: #default to r1
-        rTarget = r1
-
+    if "branch" in args[1].lower():
+        rTarget = Branch
+    elif "hq" in args[1].lower():
+        rTarget = HQ
+    else: 
+        return "Invalid Router"
+    
     #call toggle_interface and return the result
     return toggle_interface(intName,intNum, rTarget)
 
@@ -95,7 +80,7 @@ Actually toggles the interface.
     intName: The type of interface
     intNum: The interface number
     rTarget: The target router
-Returns: The state of the toggled router
+Returns: The state of the toggled interface
 '''
 def toggle_interface(intName, intNum, rTarget):
     #Put the filter used to check the state of the interface together
@@ -107,6 +92,7 @@ def toggle_interface(intName, intNum, rTarget):
     else:
         netconf_payload = int_off_template.format(int_name = intName, int_num = intNum)
 
+    print(netconf_payload)
     #Toggle the router
     rTarget.edit_config(target="running", config=netconf_payload)
     netconf_reply = rTarget.get_config(source="running", filter = netconf_filter)
