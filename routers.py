29e114053b6ecd475a17c179bcb27175429ae8c7
JacobSletten <118213062+JacobSletten@users.noreply.github.com>
2022-12-08 18:25:11 -0600

    Update routers.py

--- routers.py
+++ routers.py
@@ -1,1 +1,14 @@
-
+#Replace IP and Port if nessesary
+ncHQ = {"host":"192.168.56.101",
+            "port":"830",
+            "username":"cisco",
+            "password":"cisco123!",
+            "hostkey_verify":False
+        }
+ncBranch = {"host":"192.168.56.103",
+            "port":"830",
+            "username":"cisco",
+            "password":"cisco123!",
+            "hostkey_verify":False
+        }
+    
