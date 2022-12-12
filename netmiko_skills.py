660796ad320436db69a03f29103ff57bbbb27f3f
sansol01 <sa-sols@hotmail.no>
2022-12-12 02:58:37 -0600

    Add files via upload

--- netmiko_skills.py
+++ netmiko_skills.py
@@ -3,12 +3,9 @@ import myparamiko as m
 import time
 import threading
 import getpass
+import routers
 
-#start = time.time()
-#r1 = manager.connect(**routers.router1)
-#r2 = manager.connect(**routers.router2)
-
-r1 = {
+r1 = { # Branch router
         'device_type': 'cisco_ios',
         'host': '192.168.56.102', 
         'username': 'cisco',
@@ -17,8 +14,7 @@ r1 = {
         'secret': 'cisco123!',
         'verbose': True
     }
-
-r2 = {
+r2 = { # HQ router
         'device_type': 'cisco_ios',
         'host': '192.168.56.107', 
         'username': 'cisco',
@@ -28,40 +24,29 @@ r2 = {
         'verbose': True
     }
 
-def backup(message):
-    if "all" in message:
-        #backupR(r2)
-        #backupR(r1)
-        return ("Backing up all routers\n" + "File loaction: " + backupR(r1) + "\nFile location" + backupR(r2))
-    elif "r2" in message:
-        return "File loaction: " + backupR(r2) 
-    else: #default to r1
-        return "File location: " + backupR(r1)
+def backup(message):    # this function determines which router to backup
+
+    if "Branch" in message: # if Branch is specified, Branch will be backed up
+        return "Backup for Branch file location: " + backupR(r1)  
+    elif "HQ" in message:   # if HQ is specified, HQ will be backed up
+        return "Backup for HQ file loaction: " + backupR(r2)  
+    elif "all" in message: # is the all option is specified it will perform a backup off all the routers
+        return ("Backup of all routers performed:\n" + "Branch file loaction: " + backupR(r1) + "\n HQ file location" + backupR(r2)) # if there was no specification or all was specified all routers will be backed up
+    else:   # if no routers were specified it will give error message detailing it is case sensitive and the options availible.
+        return 'I am case-sensitive! Do "Branch" for Branch router, "HQ" for HQ router, and "all" for all rotuers.\n IMPORTANT! NO BACKUPS WERE MADE!!'
 
 
-def backupR(rTarget):
-    #cut toggle out of the string
-
-    #Check for a target router
-#    if "r2" in message:
-#        rTarget = r2
-#    else: #default to r1
-#        rTarget = r1
-
-    #call toggle_interface and return the result
-    connection = ConnectHandler(**rTarget)
-    #prompt = connection.find_prompt()
-    #if '>' in prompt:
-    connection.enable()
+def backupR(targetR):
+    connection = ConnectHandler(**targetR)  # connects to the choosen router
+    connection.enable() # makes sure that the router is in privlige exec mode
     print('Sending commands...')
-    #output = connection.send_command('copy run start')
-    output = connection.send_command('sho run')
-    output_list = output.splitlines()
-    output_list = output_list[9:-1]
+    output = connection.send_command('show run') # displays the running configuration it to the variable output
+    output_list = output.splitlines()   # splits output into seprate lines for readability
+    output_list = output_list[9:-1]     # takes away some of the unnecessary lines at the top
     output = '\n'.join(output_list)
     print(output_list)
 
-    from datetime import datetime
+    from datetime import datetime   # gets the current time and stores them to easier to use variables
     now = datetime.now()
     year = now.year
     month = now.month
@@ -69,29 +54,16 @@ def backupR(rTarget):
     hour = now.hour
     minute = now.minute
 
-    filename = f'ROUTER_BACKUP/{rTarget["host"]}_{year}-{month}-{day}-{hour}-{minute}.txt'
+    filename = f'ROUTER_BACKUP/{targetR["host"]}_{year}-{month}-{day}-{hour}-{minute}.txt'  # determines the name of the backup file using IP address and current date for version control
     with open(filename, 'w') as f:
-        f.write(output)
+        f.write(output) # writes and stores the backup config file
+
+    output = connection.send_command_timing('copy run start') # stores a local copy of the running-config to the startup-config NVRAM.
 
     print('Closing connection')
     connection.disconnect()
     print('#'*40)
 
     return filename
-
-#routers = m.get_list_from_file ('routers.txt')
-#threads = list()
-#for router in routers:
-#    th = threading.Thread(target = backup, args = (router,))
-#    threads.append(th)
-
-#for th in threads:
-#    th.start()
-
-#for th in threads:
-#    th.join()
-
-#end = time.time()
-#print(f'Total execution time: {end-start}')
 
 
